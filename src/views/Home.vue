22a66b990f40bf2073bf148dccbfe5f70060a181
Evann Gehin <evann.gehin@ezcorp.io>
2021-08-13 16:26:08 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<script>
+import Sidebar from './../components/Body/Sidebar.vue'
+import Navbar from './../components/Body/Navbar.vue'
+
+export default {
+  components: {
+    Sidebar, Navbar
+  }
+}
+</script>
